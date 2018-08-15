aadd951c3e5210aeaf318b5951b399d664efff08
Tomasz Szłapa <tomek.szlapa@gmail.com>
2018-08-15 14:57:12 +0200

    ALign with hosting structure

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -32,10 +33,12 @@ var UserSchema = new mongoose.Schema({
     }]
 })
 
+//.methods adds method that can be invoked on instance ie. user but not User
+
 //nadpisujemy funkcje moongose
 //funkcja wywoływana gdy mongoose zamienia wartośc na json/
 //ograniczamy ilość danych przesyłanych do uzytkownika
-UserSchema.methods.toJSON = function(){
+UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
 
@@ -46,18 +49,93 @@ UserSchema.methods.toJSON = function(){
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'moveittoconfig').toString();
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'moveittoconfig').toString();
 
     user.tokens = user.tokens.concat([{
         access,
         token
     }]);
 
-    return user.save().then(()=>{
+    return user.save().then(() => {
         return token;
     })
 
 }
+
+//delete one property from 'document -> row'
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    });
+}
+
+//.statics turns into model methods (invoked on User)
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'moveittoconfig');
+    } catch (e) {
+        // return new Promise((resolve, reject)=>{
+        //     reject();
+        // })
+
+        return Promise.reject();
+    }
+
+    //jeśli wartość jest zagnieżdżona to musimy właściwość wprowadzić w cudzysłowie z kropką (jak dot notation)
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    })
+
+}
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({ email }).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, resl) => {
+                if (resl) {
+                    resolve(user);
+                }
+
+                reject();
+            })
+        })
+    })
+
+}
+
+//change plain text password to hash that is stored in db
+//.pre is invoked before .save on UserSchema (instance -> user);
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        })
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User', UserSchema);
 
